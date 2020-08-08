2f6bc22fdcf92bd5fac4b731df0924d4538237fe
Andrey Kabylin <andrey@kabylin.ru>
2020-08-08 21:13:44 +0700

    db connection

--- src/app/core.clj
+++ src/app/core.clj
@@ -4,53 +4,95 @@
             [compojure.core :refer :all]
             [compojure.route :as route]
             [ring.middleware.defaults :refer :all]
+            [com.stuartsierra.component :as component]
             [clojure.pprint :as pp]
             [clojure.string :as str]
             [clojure.data.json :as json]
-            [com.stuartsierra.component :as component]))
+            [clj-postgresql.core :as pg]
+            [clojure.java.jdbc :as jdbc]))
 
-(defn- start-server [handler port]
-  (let [server (server/run-server handler {:port port})]
-    (println (str "Started serve on localhos: " port))
-    server))
+(defrecord AppState [db-connection])
 
-(defn- stop-server [server]
-  (when server
-    (server)))
+(def app-state (atom (AppState. nil)))
 
-;; (defn- init-db []
-;; )
-
-;; (def db (pg/pool :host "localhost"
-;;                  :user "clojure_crud"
-;;                  :dbname "clojure_crud"
-;;                  :password "123321"))
-
-;; (jdbc/query db ["SELECT * FROM "])
-
-;; (defn- test [] ())
-
-;; (defn- close-db [] (pg/close! db))
+(defn db-connection []
+  (:db-connection @app-state))
 
 (defn list-page [req]
   {:status  200
    :headers {"Content-Type" "text/html"}
-   :body    "Hello world!"})
+   :body (let [connection (db-connection)]
+           (jdbc/query connection ["SELECT * FROM patients"]))})
 
 (defroutes app-routes
   (GET "/" [] list-page)
   (route/not-found "Error, page not found!"))
 
-(defrecord App []
+(defn- start-http-server [port]
+  (server/run-server (wrap-defaults #'app-routes site-defaults) {:port port}))
+
+(defn- stop-server [server]
+  (when server
+    (server)))
+
+(defn- connect-to-database [host port user name password]
+  (pg/pool :host host
+           :port port
+           :user user
+           :dbname name
+           :password password))
+
+(defn- close-db-connection [connection]
+  (pg/close! connection))
+
+(defrecord Database [host port user name password, connection]
   component/Lifecycle
-  (start [this]
-    (assoc this :server (start-server (wrap-defaults #'app-routes site-defaults) 8080)))
-  (stop [this]
-    (stop-server (:server this))
-    (dissoc this :server)))
+
+  (start [component]
+    (println (str "Starting database at " host ":" port))
+    (let [connection (connect-to-database host port user name password)]
+      (swap! app-state (fn [it] (assoc it :db-connection connection)))
+      (println app-state)
+      (assoc component :connection connection)))
+
+  (stop [component]
+    (println "Stopping database")
+    (close-db-connection (:connection component))
+    (swap! app-state (fn [it] (dissoc it :db-connection)))
+    (dissoc component :connection)))
+
+(defrecord HttpServer [port]
+  component/Lifecycle
+
+  (start [component]
+    (println (str "Starting server at localhost" ":" port))
+    (let [server (start-http-server port)]
+      (assoc component :server server)))
+
+  (stop [component]
+    (println "Stopping server")
+    (stop-server (:server component))
+    (dissoc component :server)))
+
+(defn new-database [host port user name password]
+  (map->Database {:host host :port port :user user :name name :password password}))
+
+(defn new-http-server [port]
+  (map->HttpServer {:port port}))
+
+(defn crud-system [config-options]
+  (let [{:keys [db_host db_port db_user db_name db_password http_port]} config-options]
+    (component/system-map
+     :db (new-database db_host db_port db_user db_name db_password)
+     :server (new-http-server http_port))))
 
 (defn create-system []
-  (App.))
+  (crud-system {:db_host "localhost"
+                :db_port 5432
+                :db_user "clojure_crud"
+                :db_name "clojure_crud"
+                :db_password "123321"
+                :http_port 8080}))
 
 (defn -main
   [& args]
